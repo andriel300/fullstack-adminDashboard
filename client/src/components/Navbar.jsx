50a8ad7fdccc842514b303fe9b386e0e352ee9e8
andriel300 <andrielscript@gmail.com>
2023-03-21 20:44:57 -0300

    Add right-side icons to Navbar component for dark mode and settings

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,13 @@ import FlexBetween from "components/FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import profileImage from "assets/profile.jpeg";
-import { AppBar, Toolbar, useTheme, IconButton, InputBase } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  useTheme,
+  IconButton,
+  InputBase,
+} from "@mui/material";
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -42,6 +48,20 @@ const Navbar = () => {
               <Search />
             </IconButton>
           </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
         </FlexBetween>
       </Toolbar>
     </AppBar>

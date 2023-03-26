4210f51d0283df811da34a872aede9ad723c52ce
andriel300 <andrielscript@gmail.com>
2023-03-26 20:57:31 -0300

    Remove console.logs

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -7,7 +7,6 @@ import { DataGrid } from "@mui/x-data-grid";
 const Customers = () => {
   const theme = useTheme();
   const { data, isLoading } = useGetCustomersQuery();
-  console.log("🚀 ~ file: index.jsx:10 ~ Customers ~ data:", data);
 
   const columns = [
     {
